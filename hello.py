ea81639e3cc36a46292d11117064f52c4b04ba89
Abinayajayakumar <abinayajayakumar92@gmail.com>
2019-04-29 19:47:19 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+print ("Hello, Python!");
+
+def main():
+  print("inside main ,Hello World!")
+  
+main()
+print("Guru99")
+
+
+string="12345"		
+print(''.join(reversed(string)))
+
+word="guru99 career guru99"		
+print(word.split('r'))
+
+x = "Guru99"
+print(x.replace("Guru99","Python"))
+print(x)
